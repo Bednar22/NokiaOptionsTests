8e5b76798329575c22f3eaf3f1b287beb7163055
Maciek Bednarski <mBednar2020@gmail.com>
2020-03-06 20:56:02 +0100

    added 2 components, parent and child

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import Message from "./components/message.component"
 import Navbar from "./components/navbar.component";
 import CreateUser from "./components/create-user.component";
 import Counter from "./components/counter.component";
-import EventBind from "./components/clickFunction.component"
+import EventBind from "./components/clickFunction.component";
+import ParentComponent from "./components/parent.component";
+
 function App() {
   return (
     <Router>
@@ -20,8 +22,13 @@ function App() {
       <Counter/>
       <br/>
         <EventBind/>
+      <br/>
+      <ParentComponent/>
+
       </div>
     </Router>
+
+
   );
 }
 
